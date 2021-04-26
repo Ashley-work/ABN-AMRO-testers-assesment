f5d9cca0936fd3ecbf7024712aa87a23e41ef7ee
AshleyGautama <AshleyGautama@valori.nl>
2021-04-26 23:40:58 +0200

    refactor baseAction class capitalization and changes in method names

--- app/src/androidTest/java/com/abnamro/apps/referenceandroid/actions/BaseAction.java
+++ app/src/androidTest/java/com/abnamro/apps/referenceandroid/actions/BaseAction.java
@@ -9,30 +9,34 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+/**
+ * This class contains methods to return a viewAction and methods to take actions on viewActions.
+ */
+public abstract class BaseAction {
+    public ViewInteraction returnViewActionById(Integer resourceId) {
+        return onView(
+                withId(resourceId)
+        );
+    }
 
-public abstract class baseAction {
+    public ViewInteraction returnViewActionByText(String text) {
+        return onView(
+                withText(text)
+        );
+    }
+
     public void clickKebabMenu() {
         openActionBarOverflowOrOptionsMenu(getApplicationContext());
     }
 
-    public void clickButton(Integer resourceId){
-        onView(withId(resourceId))
-                .perform(click())
-                .check(matches(isDisplayed()));
-    }
-
-    public void clickButton(ViewInteraction vi){
-                vi.perform(click());
-                viewIsShown(vi);
+    public void clickButton(ViewInteraction vi) {
+        vi.perform(click());
+        viewIsShown(vi);
     }
 
     public void viewIsShown(ViewInteraction vi) {
         vi.check(matches(isDisplayed()));
-    }
-
-    public void viewIsShown(Integer resourceId) {
-        onView(withId(resourceId))
-                .check(matches((isDisplayed())));
     }
 }
